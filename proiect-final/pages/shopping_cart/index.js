5ec0d72f2c2e2c8ced5b256b1e07a788bab832c9
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-03 21:31:01 +0300

    add folders

--- proiect-final/pages/shopping_cart/index.js
+++ proiect-final/pages/shopping_cart/index.js
@@ -2,7 +2,7 @@ import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 import { useState, Fragment } from "react";
 import Product from "@/components/Product";
-import style from "../styles/Product.module.css"
+import style from "../../styles/Product.module.css"
 import Basket from "@/components/Basket";
 import App from "@/components/App";
 
