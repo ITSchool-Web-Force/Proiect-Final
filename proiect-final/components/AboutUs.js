5b4ef93c815ae433d154b5dd612472f481cdc1e1
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-06 22:21:34 +0300

    image corrections

--- proiect-final/components/AboutUs.js
+++ proiect-final/components/AboutUs.js
@@ -7,7 +7,9 @@ export default function AboutUs() {
     return <>
         <p className={styles.text}>
         Beauty and magic. The simple kind that is all around, of which we fail to see.  As long as I can remember, I have spent my time seeking it and wanting to create my own.  To share my passion and striving to always leave things better than I find them.  
-        <Image className={styles.image} src={florist_flowers} height={500} width={500} alt="florist with flowers"></Image>
+        </p>
+        <p className={styles.text}>
+        <Image className={styles.image} src={florist_flowers} height={400} width={400} alt="florist with flowers"></Image>
         When people ask me, What do you do for a living?,  Flowers  I say, smiling ear to ear.  The same smile I had as a child when my mother sent me to the garden for vegetables, watching my grandmother tend to her flowers, my father to the farm. Hands in the soil, one with the earth.
         </p>
         <Image className={styles.image} src={image_florist_field} height={500} width={500} alt="florist on a field"></Image>
