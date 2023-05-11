e4179f3c74a345ce0804d54d6da7429a80188e32
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-11 17:56:43 +0300

    update form

--- proiect-final/components/Form.js
+++ proiect-final/components/Form.js
@@ -3,23 +3,23 @@ import style from "../styles/Form.module.css"
 export default function Form() {
     return <>
         <div className={style.background}>
-        <form className={style.contact_form} name="contact-form" method="POST" data-netlify="true">
+        <form className={style.contact_form} name="contact-form" method="POST" data-netlify="true" action="contact/?success=true">
             <h1 className={style.title}>Contact us</h1>
             <label className={style.form_group}>
                 Name
-                <input type="text" name="name" placeholder="Name"/>
+                <input type="text" name="name"  required placeholder="Name"/>
             </label>
             <label class={style.form_group}>
                 Email
-            <input type="email" name="email" placeholder="user@domain.ext"/>
+            <input type="email" name="email"  required placeholder="user@domain.ext"/>
             </label>
             <label className={style.form_group}>
                 Message
-            <textarea placeholder="Contact us" name="message">
+            <textarea placeholder="Contact us" name="message" required>
             </textarea>
             </label>
             <div className={style.checkbox_group}>
-                <input type="checkbox" id="val1" name="accept"/>
+                <input type="checkbox" id="val1" name="accept" required/>
                 <label for="val1">I read the terms and conditions</label>
             </div>
             <div className={style.btn_group}>
