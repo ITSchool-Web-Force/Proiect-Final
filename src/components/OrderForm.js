5992b376a679c07b8e6aaa829e852ef34a3396eb
Elena Tabacaru <120025210+elenatabacaru@users.noreply.github.com>
2023-05-11 12:04:19 +0300

    commit

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -2,8 +2,8 @@ export default function OrderForm() {
     return (
       <div>
       <div >
-        <form name="contact"   action="/success" method="POST" data-netlify="true">
-          <p>
+        <form name="contact"   action="/" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" /> <p>
             <label htmlFor="name">Name</label>
             <input type="text" id="name" name="name" />
           </p>
