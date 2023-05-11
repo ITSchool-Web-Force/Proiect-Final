d549686972654dbdfc9ce1794d93097cee4d7db2
Roxana Iulia <roxanaiuliab@gmail.com>
2023-05-11 16:48:29 +0300

    basic version

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,115 +1,50 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function About() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Traduceri</title>
       </Head>
-
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
+        <h1 className={styles.title}>Traduceri profesionale</h1>
+          <p>Traduceri simple / autorizate.
+              Traduceri legalizate.
+              Interpretariat.</p>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+            <h1>Servicii</h1>
+            <p>Prin interpretariat, un translator poate traduce în timp real discursul tău pe care îl prezinți la o conferință, training, webinar sau orice alt eveniment.</p>
+            <h2>Traduceri legalizate</h2>
+            <p>Traducem orice text din orice limbă cu traducători nativi și specialiști lingviști, respectând standardele internaționale din domeniu pentru rezultate ideale.</p>
+            <h2>Traduceri simple / autorizate</h2>
+        </div>
+        <div>
+          <h1>Domenii de specialitate</h1>
+            <li>
+                <ul>Traduceri Medicale</ul>
+                <ul>Traduceri Juridice</ul>
+                <ul>Traduceri Automotive</ul>
+                <ul>Traduceri Financiar / Bancar</ul>
+                <ul>Traduceri Marketing / Publicitate</ul>
+            </li>
+        </div>
+        <div>
+          <h1> Haide să colaborăm!</h1>
+            <p>Lucrăm atât cu oamenii noștri cât și cu freelanceri!</p>
+        </div>
+        <div>
+          <h1>Contactează-ne!</h1>
+            <p>Str. Traduceri nr. 5
+                Timisoara, Timis
+                3007000
             </p>
-          </a>
+            <p> Tel: +407500500
+                Email: traduceri@gmail.com
+                Social: Traduceri Legale Ro
+            </p>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
