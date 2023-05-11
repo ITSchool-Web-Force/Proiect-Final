cb2fc3eab94adfd35ca1c6fa53ea87e10fa98c84
Elena Tabacaru <120025210+elenatabacaru@users.noreply.github.com>
2023-05-11 18:10:30 +0300

    comm

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -4,9 +4,9 @@ export default function Testimonial() {
   return (
  
 <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-<h2 class="text-3xl font-bold tracking-tight sm:text-4xl">
+<h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
           Don't just take our word for it...
-          <br class="hidden sm:block lg:hidden" />
+          <br className="hidden sm:block lg:hidden" />
           Read reviews from our customers
         </h2>
     <div
