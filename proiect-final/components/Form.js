1299321079e18b3517717a1f60995f65eab7a982
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-11 20:51:51 +0300

    update form

--- proiect-final/components/Form.js
+++ proiect-final/components/Form.js
@@ -1,18 +1,22 @@
+import { useState } from "react"
 import style from "../styles/Form.module.css"
 
 export default function Form() {
+    const [visible, setVisible] = useState(true);
+    
     return <>
+    {visible &&
         <div className={style.background}>
         <form className={style.contact_form} method="POST" name="contact-form" action="contact/?success=true" data-netlify="true">
             <input type="hidden" name="form-name" value="contact-form" />
             <h1 className={style.title}>Contact us</h1>
             <label className={style.form_group}>
                 Name
-                <input type="text" name="name"  required placeholder="Name"/>
+                <input type="text" name="name" placeholder="Name" required/>
             </label>
             <label class={style.form_group}>
                 Email
-            <input type="email" name="email"  required placeholder="user@domain.ext"/>
+            <input type="email" name="email" placeholder="user@domain.ext" required/>
             </label>
             <label className={style.form_group}>
                 Message
@@ -25,9 +29,21 @@ export default function Form() {
             </div>
             <div className={style.btn_group}>
                 <button className={style.button_style} type="reset" value="Reset the form">Reset the form</button>
-                <button className={style.button_style} type="submit" id="submit" value="Trimite">Send</button>
+                <button className={style.button_style} type="submit" id="submit" value="Send"
+                 onClick={(event) => setVisible(!visible)}>
+                 Send
+                </button>
             </div>
         </form>
-        </div>
+        </div>}
+        {!visible &&
+            <div className={style.form_message}>
+                <div className={style.thanks_message}>Thank you, your message was sent</div>
+                <button className={style.button_style} type="submit" id="submit" value="Send"
+                onClick={(event) => setVisible(!visible)}>
+                Back to a new form
+                </button>
+            </div>
+        }
     </>
 }
