31c31d55f51fa3873464811df799c3e53cd96078
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-05-11 19:13:53 +0300

    fileroutechange

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
-import { useGetUserID } from "./hooks/useGetUSerID.js";
+import { useGetUserID } from "@/hooks/useGetUserID";
 import { useRouter } from "next/router";
 
   
