cda8988751fc4d163e745e4341170cb5e147582e
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-14 16:33:17 +0300

    modify form name

--- proiect-final/components/CartItem.js
+++ proiect-final/components/CartItem.js
@@ -3,13 +3,14 @@ import Image from "next/image"
 import { ShoppingCartContext } from "@/context/ShoppingCartContext";
 import { useContext } from "react";
 
-export default function CartItem ({price, name, image, id}) {
+export default function CartItem ({price, name, image, id, key}) {
   const {addProducts, removeProducts, cartItems} = useContext(ShoppingCartContext);
   return <>
     <div className={style.product_container}>
     <div className={style.cart_product}>
       <div className={style.name_price}>
       <div className={style.product_name}></div>
+      {key}
         <div className={style.product_name}>{name}</div>
         <div className={style.product_price}>{price}$</div>
       </div>
