cb9aba65af35d65b75643dae8771fbb44fc2451b
Cervencu George <120504020+GGC911@users.noreply.github.com>
2023-05-15 18:49:26 +0300

    form test

--- auto-dealer/components/MyForm.js
+++ auto-dealer/components/MyForm.js
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 function MyForm() {
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsFormSubmitted(true);
+  };
   return (
     <div className="form-container">
       <h3>GET IN TOUCH</h3>
@@ -12,6 +20,7 @@ function MyForm() {
         data-netlify="true"
         action="contact/?success=true"
         data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
       >
         <div className="form-layout">
           <div className="form-group">
@@ -39,6 +48,13 @@ function MyForm() {
             <button className="send-button" type="submit">
               SEND MESSAGE
             </button>
+            {isFormSubmitted ? (
+              <h4>
+                Thank you for your message. We will get back to you shortly
+              </h4>
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </form>
