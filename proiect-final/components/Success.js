38a6542dc9fff366949a4111eeb72ed61b2d5e37
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-15 23:07:25 +0300

    success message

---
+++ proiect-final/components/Success.js
@@ -0,0 +1,5 @@
+export default function Success() {
+    return <>
+    <div>Thank you for your order</div>
+    </>
+}
---
+++ proiect-final/pages/success/index.js
@@ -0,0 +1,11 @@
+import { Nav } from "react-bootstrap";
+import Footer from "@/components/Footer";
+import Success from "@/components/Success"
+
+export default function SuccessMessage() {
+    return <>
+    <Nav />
+    <Success />
+    <Footer />
+    </>
+}
