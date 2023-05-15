4bf7aa598a2c16185d53e414f0bbe460ef8cd04d
Cervencu George <120504020+GGC911@users.noreply.github.com>
2023-05-15 20:47:01 +0300

    modal test

--- auto-dealer/components/EnquiryModal.js
+++ auto-dealer/components/EnquiryModal.js
@@ -18,7 +18,7 @@ function EnquiryModal(props) {
           name="enquire-form"
           method="POST"
           data-netlify="true"
-          action="currents-stock/1/?success=true"
+          action="current-stock/1/?success=true"
           data-netlify-honeypot="bot-field"
         >
           <div className="form-layout">
