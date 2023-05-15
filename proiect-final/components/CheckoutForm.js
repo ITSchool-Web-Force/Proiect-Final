9a8a2c24a4070b9bd8dc905f5e7d64c1dd9c5091
Diana-Elena Dinu <diana.elena_dinu@yahoo.com>
2023-05-15 20:17:07 +0300

    update checkout form

--- proiect-final/components/CheckoutForm.js
+++ proiect-final/components/CheckoutForm.js
@@ -1,75 +1,36 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
-import Cart from './Cart';
-import style from "../styles/CheckoutForm.module.css"
+import style from "../styles/Form.module.css"
 
-export default function CheckoutForm (props) {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const {totalAmount} =  props;
-
-  return (
-    <>
+export default function CheckoutForm() {
+  const [visible, setVisible] = useState(true);
+  
+  return <>
     <form method="POST" name="checkout-contact-form" action="contact/?success=true" data-netlify="true" data-netlify-honeypot="bot-field">
-      <input type="hidden" name="checkout-form-name" value="checkout-contact-form" />
-      <Button variant="success" onClick={handleShow}>
-        Checkout
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Fill in the details for your order</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Your name"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Your address"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Your comments</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <p className={style.total_amount}>Total amount to pay: {totalAmount}$</p>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button className={style.button_style} type="submit" id="submit" value="Send"
-            variant="secondary" onClick={handleClose}>
-            Send
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      </form>
-    </>
-  );
+    <input type="hidden" name="checkout-form-name" value="checkout-contact-form" />
+    <h1 className={style.title}>Contact us</h1>
+        <label className={style.form_group}>
+            Name
+            <input type="text" name="name" placeholder="Name" required/>
+        </label>
+        <label class={style.form_group}>
+            Email
+        <input type="email" name="email" placeholder="user@domain.ext" required/>
+        </label>
+        <label className={style.form_group}>
+            Message
+        <textarea placeholder="Contact us" name="message" required>
+        </textarea>
+        </label>
+        <div className={style.checkbox_group}>
+            <input type="checkbox" id="val1" name="accept" required/>
+            <label for="val1">I read the terms and conditions</label>
+        </div>
+        <div className={style.btn_group}>
+            <button className={style.button_style} type="reset" value="Reset the form">Reset the form</button>
+            <button className={style.button_style} type="submit" id="submit" value="Send"
+              onClick={(event) => setVisible(!visible)}>
+              Send
+            </button>
+        </div>
+    </form>
+  </>
 }
