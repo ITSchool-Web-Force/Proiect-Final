bc50b2307e496d078dec4490bae3c39e6c3464c8
Crușoveanu Ioan <ionut.cruso@gmail.com>
2023-05-16 22:14:10 +0300

    theme fix

--- components/header/ThemeSwitch.js
+++ components/header/ThemeSwitch.js
@@ -1,5 +1,7 @@
 import { useTheme } from "next-themes";
 
+import { useEffect } from "react";
+
 import Switch from "react-switch";
 import NoSSRWrapper from "../NoSSRWrapper"
 
@@ -9,10 +11,20 @@ import Lightmode from "../../svgs/light";
 import styles from "../../styles/header/switcher.module.scss";
 
 function ThemeSwitch() {
+    
     const { theme, setTheme } = useTheme();
+
+    useEffect(() => {
+        const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        if (theme === "system" && typeof window !== "undefined") {
+            setTheme(prefersDarkTheme ? "dark" : "light");
+        }
+    }, [theme, setTheme]);
+
     const toggle = () => {
-        setTheme(theme === "light" ? "dark" : "light")
-    }
+        setTheme(theme === "light" ? "dark" : "light");
+    };
+
     return <> 
         <NoSSRWrapper>
             <div className={styles.toggleButton}>
