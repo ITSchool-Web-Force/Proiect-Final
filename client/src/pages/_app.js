f427ae6cfd774023094da96da030451f76cb2487
Alex Silviu Scripcariuc <alex.silviu.scripcariuc@gmail.com>
2023-05-16 15:22:43 +0300

    Footer in Motion fix

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -34,8 +34,8 @@ export default function App({ Component, pageProps, router }) {
       {shouldRenderNavbar && <Navbar />}
       <AnimatePresence mode="wait" initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
         <Component {...pageProps} key={router.asPath} />
+        {shouldRenderNavbar && <Footer />}
       </AnimatePresence>
-      {shouldRenderNavbar && <Footer />}
     </>
   );
 }
